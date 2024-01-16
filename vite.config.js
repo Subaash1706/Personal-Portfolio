3db0fcd4edabd8974b459ade0ee80f3636b4fe25
Subaash1706 <subaashbala@gmail.com>
2024-01-16 22:55:11 +0530

    fix: deployment issue

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/Personal-Portfolio/',
+  plugins: [react()],
   server:{
     host: '0.0.0.0',
   },
