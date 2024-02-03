c3360160062c4e8da52a4beeb889e9bd530e617a
Subaash1706 <subaashbala@gmail.com>
2024-02-03 16:56:38 +0530

    fix: logic to disabled form upon submission

--- src/components/Contact/Form.jsx
+++ src/components/Contact/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import classes from './form.module.css'
 import Button from '../Button/Button'
 import emailjs from '@emailjs/browser'
@@ -6,11 +6,13 @@ import _default from '@emailjs/browser'
 
 
 function Form() {
-    const [ formResponseStatus, setFormResponseStatus ] = useState(false)
+    const [ formResponseStatus, setFormResponseStatus ] = useState(false);
+    const [ disabledButton, setDisableButton ] = useState(false);
     const [ name, setName ] = useState('');
     const formRef = useRef()
     function formSubmitHandler(e){
         e.preventDefault()
+        setDisableButton(true);
         emailjs.sendForm('service_6d741yg', 'template_np60r8d', formRef.current, 'U2qIQCz2e4LikqCFC')
         .then((res)=>{
             if(res.status === 200){
@@ -24,6 +26,9 @@ function Form() {
             console.log(err.text)
         }
     }
+    useEffect(()=>{
+        if(formResponseStatus) setDisableButton(false)
+    }, [ formResponseStatus ])
   return (
     <>
         {
@@ -34,17 +39,17 @@ function Form() {
         }
         {!formResponseStatus && <form className={classes.contactForm} onSubmit={formSubmitHandler} ref={formRef} >
             <div style={{display: 'flex'}}>
-                <input type="text" placeholder="What's your First name?" name='first_name' style={{borderRight: 'none', borderBottom: 'none'}} onChange={(e)=>setName(e.target.value)} value={name}/>
+                <input type="text" placeholder="What's your First name?" name='first_name' style={{borderRight: 'none', borderBottom: 'none'}} onChange={(e)=>setName(e.target.value)} value={name} required/>
                 <input type="text" placeholder="What's your Second name?" name='second_name' style={{borderBottom: 'none'}}/>
             </div>
             <div className={classes.textAreaParaContainer}>
                 <div className={classes.inputParaContainer}>
-                    <input type="email" placeholder="What's your Email Id?" name='email' className={classes.emailInput}/>
+                    <input type="email" placeholder="What's your Email Id?" name='email' className={classes.emailInput} required/>
                 </div> 
-                <textarea name="message" id="message" cols="30" rows="5" placeholder='Enter your message' style={{borderBottom: 'none'}}></textarea>
+                <textarea name="message" id="message" cols="30" rows="5" placeholder='Enter your message' style={{borderBottom: 'none'}} required></textarea>
             </div>
             <center style={{textAlign: 'center'}} className={classes.buttonContainer}>
-                <Button labelName={'Send message'} style={{width: '25%', marginBlock:'1rem'}} color={'rgba(105, 210, 231, 1)'} disabled={name.length > 0 ? false : true}/>
+                <Button labelName={!disabledButton ? 'Send message' : 'Sending message'} style={{width: '25%', marginBlock:'1rem'}} color={'rgba(105, 210, 231, 1)'} disabled={(name.length > 0 ? false : true) || disabledButton }/>
             </center>
         </form>}
     </>
