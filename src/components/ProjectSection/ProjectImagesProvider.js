391c6b1950de9705675f63fa833d857434772c59
Subaash1706 <subaashbala@gmail.com>
2024-02-03 19:54:03 +0530

    fix: image path

--- src/components/ProjectSection/ProjectImagesProvider.js
+++ src/components/ProjectSection/ProjectImagesProvider.js
@@ -2,8 +2,8 @@ const project1Array = [ 'image1' ]
 const project2Array = [ 'image1' ]
 const project3Array = [ 'image1' ]
 
-const project1 = project1Array.map((item, ind)=>`/Personal-Portfolio/assets/images/project1/${item}.png`)
-const project2 = project2Array.map((item, ind)=>`/Personal-Portfolio/assets/images/project2/${item}.png`)
-const project3 = project3Array.map((item, ind)=>`/Personal-Portfolio/assets/images/project3/${item}.png`)
+const project1 = project1Array.map((item, ind)=>`/assets/images/project1/${item}.png`)
+const project2 = project2Array.map((item, ind)=>`/assets/images/project2/${item}.png`)
+const project3 = project3Array.map((item, ind)=>`/assets/images/project3/${item}.png`)
 
 export { project1, project2, project3 }
