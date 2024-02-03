1a8b5b96723620720c91d3cd4925b872b36cdd4c
Subaash1706 <subaashbala@gmail.com>
2024-02-03 16:57:22 +0530

    style: minor UX tweaks

--- src/components/ProjectSection/IndividualProjectItem.jsx
+++ src/components/ProjectSection/IndividualProjectItem.jsx
@@ -8,12 +8,13 @@ const IndividualProjectItem = (props) => {
         '#69d2e7', '#7fbc8c', '#ff6b6b', '#ff69b4', 'c4a1ff', '#e3dff2', '#ffff00', '#a388ee','#7df9ff', '#ff4911'
     ]
     const shortPalette = [
-        '#7fbc8c', '#a388ee', '#ffa07a','#ff6b6b'
+        '#DCFFB7', '#7BD3EA', '#ffbb64','#ff6b6b'
     ]
     function cursorMoveHandler(e, image){
         const x = e.clientX;
         const y = e.clientY;
         const cursor = document.querySelector('#cursor')
+        const subCursor = document.querySelector('#subCursor');
         const wrapper = document.querySelector('#projectWrapper')
         const wrapperX = wrapper.getBoundingClientRect().x;
         const wrapperY = wrapper.getBoundingClientRect().y;
@@ -21,18 +22,27 @@ const IndividualProjectItem = (props) => {
         cursor.style.top = `${y-wrapperY}px`
         setCursorImage(image)
         cursor.style.background = `url(${cursorImage})`;
-        cursor.style.backgroundSize = 'cover'
+        cursor.style.backgroundSize = 'cover';
+        subCursor.style.left = `${x-wrapperX}px`
+        subCursor.style.top = `${y-wrapperY}px`
+        subCursor.style.backgroundColor = shortPalette[e.target.id.split('-')[1]]
     }
     function cursorLeaveHandler(e){
         const cursor = document.querySelector('#cursor')
+        const subCursor = document.querySelector('#subCursor');
         cursor.style.width = '0px';
+        subCursor.style.scale = '0';
         cursor.style.opacity = '0';
+        subCursor.style.opaicty = '0';
         cursor.style.border = 'none'
     }
     function cursorEnterHandler(e){
         const cursor = document.querySelector('#cursor')
+        const subCursor = document.querySelector('#subCursor');
         cursor.style.width = '60vh';
+        subCursor.style.scale = '1';
         cursor.style.opacity = '1';
+        subCursor.style.opacity = '1';
         cursor.style.border ='4px solid black'
     }
     function cursorClickHandler(e, link){
@@ -52,7 +62,7 @@ const IndividualProjectItem = (props) => {
                             onMouseMove={(e)=>cursorMoveHandler(e, ...item.imageSrc)}  
                             onMouseEnter={cursorEnterHandler} 
                             onClick={(e)=>cursorClickHandler(e, item.link)} 
-                            id={`indItem${ind}`} 
+                            id={`indItem-${ind}`} 
                             style={{backgroundColor: shortPalette[ind]}}
                         >
                             <div className={styles.projImage} style={{background: `url(${item.imageSrc[0]}) 0% 0% /contain`}}></div>
@@ -71,6 +81,7 @@ const IndividualProjectItem = (props) => {
             })
         }
         <div className={styles.cursor} id='cursor'></div>
+        <div className={styles.subCursor} id='subCursor'>view</div>
         <div style={{position: 'absolute', left: '-9999px', top: '-9999px', opacity: '0', visibility: 'hidden'}}>
             {props.items.map((item, ind)=>{
                 return(<div key={ind} style={{background: `url(${item.imageSrc[0]}) 0% 0% / contain`, width: '30vw', aspectRatio : '1'}}></div>)
