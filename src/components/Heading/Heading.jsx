5fb94e52805588f9ee711e298c80f2a008773259
Subaash1706 <subaashbala@gmail.com>
2024-02-07 15:13:50 +0530

    style: included fallback animations for browsers that don't support animation-timeline

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import classes from './heading.module.css'
 import circle from '../../../public/assets/svgs/circle.svg'
 
 function Heading(props) {
+  const [ featureSupport, setFeatureSupport ] = useState(true)
+  useEffect(()=>{
+    if(!('animationTimeline' in document.createElement('div').style)){
+        setFeatureSupport(false);
+    }
+  }, [])
   return (
     <div className={classes.headingWrapper} style={{filter: props.intersecting ? 'brightness(1) ': 'brightness(0.25)'}} >
-      <div className={classes.scrollWrapper}>
+      <div className={`${(!featureSupport && props.intersecting) ? 'slideLeft' : ''} ${classes.scrollWrapper}`}>
         {props.innerHTML}
         <span>
           <img src={circle}/>
--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -8,12 +8,25 @@ import full from '../../../public/assets/images/fullscreen.gif'
 function HeroSection(props) {
   const [ toggleTip, setToggleTip ] = useState(true)
   const [ heroAnimationState, setHeroAnimationState ] = useState(true);
+  const [ scrollY, setScrollY ] = useState(1);
+  const [ featureSupport, setFeatureSupport ] = useState(true);
   setTimeout(() => {
       setToggleTip(false)
   }, 10000);
   useEffect(()=>{
     (window.innerHeight < (+window.scrollY)) ? setHeroAnimationState(false) : setHeroAnimationState(true);     
   }, [ window.scrollY ])
+
+  useEffect(()=>{
+    const circle = document.getElementById('circle');
+      if(!('animationTimeline' in circle.style)){
+        setFeatureSupport(false);
+        if(+window.scrollY > 0 && +window.scrollY <= 600){
+          setScrollY((0.02 * (window.scrollY/2)));
+        }
+        else if(+window.scrollY === 0) setScrollY(1);
+      }
+  }, [window.scrollY])
   const rootElement = document.documentElement;
   if(heroAnimationState){
     rootElement.style.setProperty('--BACKGROUND-ANIMATION-PLAYSTATE', 'running');
@@ -35,7 +48,7 @@ function HeroSection(props) {
       </div> 
       <div className={classes.avatarSection}>
       </div>
-        <div className={classes.circle} ></div>
+        <div className={`${classes.circle} circle`} id='circle' style={{transform: !featureSupport ? `translate3d(-50%, -0%, 0) scale(${scrollY})` : 'translate3d(-50%, 0, 0)', transition: !featureSupport ? 'transform 250ms ease-out' : ''}}></div>
       {toggleTip && <div className={classes.mouseAnimationContainer}>
         <div>
           <img className={classes.mouseAnimation} src={mouse} />
