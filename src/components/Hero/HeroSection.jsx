21c35ec498a69a32200bb9c288d3122feb105c8c
Subaash1706 <subaashbala@gmail.com>
2024-02-27 11:31:47 +0530

    improve accessibility

--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from 'react'
 import classes from './hero.module.css'
 import nameSvg from '../../../public/assets/svgs/nameredesignoutlined.svg'
 import strokeSvg from '../../../public/assets/svgs/namestrokeshadow.svg'
-import mouse from '../../../public/assets/images/mouse.gif'
-import full from '../../../public/assets/images/fullscreen.gif'
+import scrollArrow from '../../../public/assets/svgs/scrolldownarrow.svg'
 
 function HeroSection(props) {
   const [ toggleTip, setToggleTip ] = useState(true)
@@ -35,31 +34,23 @@ function HeroSection(props) {
   return (
     <div className={classes.heroSectionWrapper} id='heroSection'>
       <div className={classes.marqueeNameWrapper} style={{animationPlayState: heroAnimationState ? 'running' : 'paused'}}>
-        <img src={nameSvg} className={classes.marqueeName}/>
-        <img src={nameSvg} className={classes.marqueeName}/>
-        <img src={nameSvg} className={classes.marqueeName}/>
-        <img src={nameSvg} className={classes.marqueeName}/>
+        <img src={nameSvg} className={classes.marqueeName} alt='Subaash'/>
+        <img src={nameSvg} className={classes.marqueeName} alt='Subaash'/>
+        <img src={nameSvg} className={classes.marqueeName} alt='Subaash'/>
+        <img src={nameSvg} className={classes.marqueeName} alt='Subaash'/>
       </div>
       <div className={classes.marqueeStrokedNameWrapper} style={{animationPlayState: heroAnimationState ? 'running' : 'paused'}}>
-        <img src={strokeSvg} />
-        <img src={strokeSvg} />
-        <img src={strokeSvg} />
-        <img src={strokeSvg} />
+        <img src={strokeSvg} alt='Subaash stroked text'/>
+        <img src={strokeSvg} alt='Subaash stroked text'/>
+        <img src={strokeSvg} alt='Subaash stroked text'/>
+        <img src={strokeSvg} alt='Subaash stroked text'/>
       </div> 
       <div className={classes.avatarSection}>
       </div>
         <div className={`${classes.circle} circle`} id='circle' style={{transform: !featureSupport ? `translate3d(-50%, -0%, 0) scale(${scrollY})` : 'translate3d(-50%, 0, 0)', transition: !featureSupport ? 'transform 250ms ease-out' : ''}}></div>
-      {toggleTip && <div className={classes.mouseAnimationContainer}>
-        <div>
-          <img className={classes.mouseAnimation} src={mouse} />
-          <span>Scroll</span>
+        <div className={classes.mouseAnimationContainer}>
+            <img src={scrollArrow} alt='Scroll down'/>
         </div>
-        <div>
-          <img className={classes.mouseAnimation} src={full} />
-          <span>Hit F11</span>
-        </div>
-
-      </div>}
     </div>
   )
 }
--- src/components/Landing/LandingPage.jsx
+++ src/components/Landing/LandingPage.jsx
@@ -10,20 +10,26 @@ function LandingPage({progress}) {
   const [ reveal, setReveal ] = useState(false);
   const [ loadHide, setLoadHide ] = useState(false);
   const [ shrinkState, setShrinkState ]= useState(false);
-  const [ layer, setLayer ] = useState(900)
+  const [ layer, setLayer ] = useState(900);
+  const [ hideDevCircle, setHideDevCircle ] = useState(false);
+
   function moveCursor(e){
     if(!reveal && cursorState){
+      const cursor = document.getElementById('loaderCursor');
     const inner = document.getElementById('innerCircle');
     const innerX = inner.getBoundingClientRect().x;
     const innerY = inner.getBoundingClientRect().y;
-    const cursor = document.getElementById('loaderCursor');
+    const cinnerX = cursor.getBoundingClientRect().x;
+    const cinnerY = cursor.getBoundingClientRect().y;
+    const cwidht = cursor.getBoundingClientRect().width;
+    const cheight = cursor.getBoundingClientRect().height;
     cursor.style.top = `${+e.clientY - innerY}px`;
     cursor.style.left = `${+e.clientX - innerX}px`
+    // console.log(`translate(-50*${e.clientY - innerY - (cwidht / 2)}%, -50*${e.clientX - innerX - (cheight / 2)}%)`)
   }
   }
   const { imageState: image, docState: doc } = progress;
   function renderDynamicCursorText(){
-    // alert(`image: ${image}, doc: ${doc}`);
     if(!image && !doc) {
       return 'Loading assets'
     }
@@ -53,9 +59,12 @@ function LandingPage({progress}) {
   }, [ loadHide ])
   useEffect(()=>{
     if(shrinkState){
-      setLayer(100)
+      setLayer(100);
+      setTimeout(() => {
+        setHideDevCircle(true);
+      }, 3500);
     }
-  })
+  }, [ shrinkState ])
   const docEle = document.documentElement;
   const root= document.getElementById('root');
   !shrinkState ? (docEle.style.setProperty('--ROOT-MAX-HEIGHT', '100vh'), docEle.style.setProperty('--SCROLL-WIDTH', '0px')) : (docEle.style.setProperty('--ROOT-MAX-HEIGHT', 'unset'), docEle.style.setProperty('--SCROLL-WIDTH', '10px'));
@@ -65,44 +74,43 @@ function LandingPage({progress}) {
     {!loadHide && <div className={`${classes.container}`} id='loadContainer'>
       <div className={classes.landingPageContainer}>
           <div className={classes.landingPageImageContainer}>
-            <img src={welcome} />
-            <img src={welcome} />
-            <img src={welcome} />
-            <img src={welcome} />
+            <img src={welcome} alt='Welcome text'/>
+            <img src={welcome} alt='Welcome text'/>
+            <img src={welcome} alt='Welcome text'/>
+            <img src={welcome} alt='Welcome text'/>
           </div>
           <div className={classes.landingPageImageContainer}>
-            <img src={wish} />
-            <img src={wish} />
-            <img src={wish} />
-            <img src={wish} />
+            <img src={wish} alt='Greeting'/>
+            <img src={wish} alt='Greeting'/>
+            <img src={wish} alt='Greeting'/>
+            <img src={wish} alt='Greeting'/>
           </div>
           <div className={classes.landingPageImageContainer}>
-            <img src={welcome} />
-            <img src={welcome} />
-            <img src={welcome} />
-            <img src={welcome} />
+            <img src={welcome} alt='Welcome text'/>
+            <img src={welcome} alt='Welcome text'/>
+            <img src={welcome} alt='Welcome text'/>
+            <img src={welcome} alt='Welcome text'/>
           </div>
           <div className={classes.landingPageImageContainer}>
-            <img src={wish} />
-            <img src={wish} />
-            <img src={wish} />
-            <img src={wish} />
+            <img src={wish} alt='Greeting'/>
+            <img src={wish} alt='Greeting'/>
+            <img src={wish} alt='Greeting'/>
+            <img src={wish} alt='Greeting'/>
           </div>
       </div>
       <div className={`${classes.circle}`}>
-        <img src={(image && doc) ? ready : loading} loading='eager'/>
+        <img src={(image && doc) ? ready : loading} loading='eager' alt='Loading circle'/>
         <div className={`${classes.innerCircle}`} onMouseMove={moveCursor} onMouseLeave={()=>setCursorState(false)} onMouseEnter={()=>setCursorState(true)} id='innerCircle'>
           <div className={`${classes.cursor} ${reveal && classes.zoomInOut}`} id='loaderCursor' style={{top: !cursorState && '50%', left: !cursorState && '50%'}} onClick={revealSiteContent}>
-            { !reveal ? renderDynamicCursorText() : '' }
+              { !reveal ? renderDynamicCursorText() : '' }
           </div>
         </div>
       </div>
     </div>}
     {
     loadHide && 
-      <div className={`${classes.postLoadContainer} `}>
-        <div className={`${classes.postLoadContainerCursor} ${shrinkState && classes.shrink}`} style={{zIndex: layer}}>
-
+      <div className={`${classes.postLoadContainer}`} style={{ display: hideDevCircle ? 'none' : 'block'}}>
+        <div className={`${classes.postLoadContainerCursor} ${shrinkState && classes.shrink}`} style={{zIndex: layer}}> 
         </div>
       </div>
     }
