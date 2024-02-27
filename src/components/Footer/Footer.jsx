6144778c9000523ffe05319c91e5a74f2a63d44f
Subaash <91363252+Subaash1706@users.noreply.github.com>
2024-02-27 16:20:49 +0530

    style: update Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,9 +9,6 @@ function Footer() {
             <span>Embrace the Imperfection</span>
             <br />
             <span>Layout: NeuBrutalism</span>
-            <a href="https://www.buymeacoffee.com/Subaash" target="_blank" className={classes.coffeeLink}>
-              <img src={coffee} alt="Buy me a coffee"/>
-            </a>
         </div>
         <div style={{textAlign: 'right'}} className={classes.second}>
             <span>Built using React & CSS in Tamil Nadu, India</span>
